7eaba56aca016ee492a0f76795144cc8a36db9c4
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-06-07 14:25:47 +0700

    upload

---
+++ coba.js
@@ -0,0 +1,7 @@
+console.log("Hello world");
+
+function cetakNama(nama) {
+  return `Halo ${nama}`;
+}
+
+module.exports = cetakNama;
---
+++ index.js
@@ -0,0 +1,10 @@
+// Video 5
+
+// const nama = "Fajar";
+// const cetakNama = (nama) => `Hi ${nama}`;
+// console.log(cetakNama(nama));
+
+const cetakNama = require("./coba");
+
+console.log("Hello WPU");
+console.log(cetakNama("Fajar"));
